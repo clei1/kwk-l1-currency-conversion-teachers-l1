631d0e4fc466a23d7b704534d8ba4515e5c974ba
clei1 <callmecon328@gmail.com>
2018-07-18 23:32:21 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -31,11 +31,24 @@ def aud_to_usd(m)
   return m * 1.2
 end
 
+puts "How much money do you want to convert?"
+amount = gets.chomp().to_i
+ 
+puts "Pick from the following menu:"
+puts "1. USD to EUR"
+puts "2. USD to JPY"
+puts "3. USD to GBP"
+puts "4. USD to AUD"
+puts "5. EUR to USD"
+puts "6. JPY to USD"
+puts "7. GBP to USD"
+puts "8. AUD to USD"
+
 choice = gets.chomp().to_i
 
 case choice
 when 1
-  
+  usd_to_aud(choice)
 when 2
   #code here
 when 3
