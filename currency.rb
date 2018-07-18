80bd588899a6d23ebe774aaa63700b127618df07
clei1 <callmecon328@gmail.com>
2018-07-18 23:29:16 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -6,3 +6,27 @@ end
 def eur_to_usd(m)
   return m * 1.2
 end
+
+def usd_to_jpy(m)
+  return m / 1.2
+end
+
+def jpy_to_usd(m)
+  return m * 1.2
+end
+
+def usd_to_gbp(m)
+  return m / 1.2
+end
+
+def gbp_to_usd(m)
+  return m * 1.2
+end
+
+def usd_to_aud(m)
+  return m / 1.2
+end
+
+def aud_to_usd(m)
+  return m * 1.2
+end
